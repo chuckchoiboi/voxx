198fab11adca0977a4a4f0e81457150f8df76cf5
Chuck Choi <hyunchoi94@gmail.com>
2024-10-27 16:41:26 -0700

    test connection with env var setting

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Image, StyleSheet, Platform } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
@@ -5,7 +6,19 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
+import { fetchJournalEntries } from '../../models/JournalModel';
+
 export default function HomeScreen() {
+	useEffect(() => {
+		fetchJournalEntries().then((entries) => {
+			if (entries) {
+				console.log('Fetched entries:', entries);
+			} else {
+				console.log('No entries found or an error occurred.');
+			}
+		});
+	}, []);
+
 	return (
 		<ParallaxScrollView
 			headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,6 +1,12 @@
 // src/services/supabaseClient.ts
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+	throw new Error('Supabase environment variables are not set');
+}
 
 export const supabase: SupabaseClient = createClient(
 	SUPABASE_URL,
